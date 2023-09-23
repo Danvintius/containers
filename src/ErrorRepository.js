57818a1f390944de77b9415cbf190a8cfc2d0019
Danvintius <danila.94@mail.ru>
2023-09-23 21:47:41 +0300

    1

---
+++ src/ErrorRepository.js
@@ -0,0 +1,13 @@
+class ErrorRepository {
+    constructor() {
+        this.errors = new Map();
+    }
+
+    translate(code) {
+        if (code === null) {
+            return 'Unknown error';
+        } else {
+            this.errors.get(code);
+        }
+    }
+}
---
+++ src/Team.js
@@ -0,0 +1,22 @@
+class Team {
+    constructor() {
+        this.members = new Set();
+    }
+    
+    add(character) {
+            if (this.members.has(character)) {
+                    throw new Error("Этот персонаж уже есть");
+            } else {
+                this.members.add(character);
+            }
+        
+}
+
+    addAll(...characters) {
+        this.members.add(...characters)
+}
+
+     toArray() {
+
+}
+}
